81083ce3f5037d4f6951cf3c3293254ca9397ebc
Daniel <62781672+dwernz@users.noreply.github.com>
2023-05-17 15:25:46 -0400

    Add: questions to test component

--- test_components/Flashcards/questions.js
+++ test_components/Flashcards/questions.js
@@ -9,23 +9,23 @@ const qaObject = [
         answer: "B) Toner cartridge<br><br>Ghosting can be caused by a broken cleaning blade or malfunctioning erasure lamps. A broken cleaning blade can be addressed by replacing the toner cartridge, which is a cheaper option than replacing erasure lamps and therefore should be tried first.<br><br>A problem related to the EP drum is vertical black lines. A problem related to the transfer corona could be blank pages.",
     },
     {
-        question: "",
-        answer: "",
+        question: "A user notices that a laser printer is having paper jams after the paper reaches the transfer corona assembly. What piece of equipment could MOST LIKELY be malfunctioning that is causing a problem at this point?<br><br>A) EP drum<br>B) High-voltage power supply<br>C) Static-charge eliminator strip<br>D) Toner cartridge",
+        answer: "C) Static-charge eliminator strip<br><br>The static-charge eliminator strip takes the transfer corona charge away from the paper. Without the strip, the charge may stay on the paper, causing it to stick to the EP cartridge.<br><br>A problem with the high-voltage power supply would result in the printer not powering on. A problem with the toner cartridge could result in blank pages. A problem with the EP drum could result in repetitive marks and defects in prints.",
     },
-    {
-        question: "",
-        answer: "",
-    },
-    {
-        question: "",
-        answer: "",
-    },
-    {
-        question: "",
-        answer: "",
-    },
-    {
-        question: "",
-        answer: "",
-    },
+    // {
+    //     question: "",
+    //     answer: "",
+    // },
+    // {
+    //     question: "",
+    //     answer: "",
+    // },
+    // {
+    //     question: "",
+    //     answer: "",
+    // },
+    // {
+    //     question: "",
+    //     answer: "",
+    // },
 ];
