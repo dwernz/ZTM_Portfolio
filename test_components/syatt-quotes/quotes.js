edc2ab26a0188df87528bc69edac7848df795e44
Daniel <62781672+dwernz@users.noreply.github.com>
2023-06-14 13:58:48 -0400

    Add: SYATT Quotes

---
+++ test_components/syatt-quotes/quotes.js
@@ -0,0 +1,122 @@
+const localQuotes = [
+    {
+        text: 'You can get everything in life you want if you help enough other people get what they want.',
+        author: 'Zig Ziglar'
+    },
+    {
+        text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        text: "Man was designed for accomplishment, engineered for success, and endowed with the seeds of greatness.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You are the only one who can use your ability. It is an awesome responsibility.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "No one is smart enough to remember everything he knows.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "Ability is important - dependability is critical.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "The real opportunity for success lies within the person and not in the job; that you can best get to the top by getting to the bottom of things.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "An in-depth look reveals considerably more than a casual glance.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "Most of us occasionally need someone to point out the obvious and more often, the not so obvious.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "The price of success is much lower than the price of failure.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You already have every characteristic necessary for success.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: `Your "natural resources," unlike the natural resources on planet Earth, will be wasted and "used up" only if they are never used at all.`,
+        author: "Zig Ziglar"
+    },
+    {
+        text: "A dime and a $20 gold piece have the same value if they are corroding at the bottom of the ocean.",
+        author: "Major Reuben Siverling"
+    },
+    {
+        text: "The biggest tragedy in America is not the great waste of natural resources, though this is tragic. The biggest tragedy is the waste of human resources.",
+        author: "Oliver Wendell Holmes"
+    },
+    {
+        text: "The average person goes to his grace with his music still in him.",
+        author: "Oliver Wendell Holmes"
+    },
+    {
+        text: "You are where you are because that's exactly where you want to be.",
+        author: null
+    },
+    {
+        text: "Today is the first day of the rest of your life.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "Winning is not everything, but the effort to win is.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "Your Success and your happiness start with you.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You already have the ability necessary for success.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "In your hands you hold the seeds of failure - or the potential for greatness.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "Success is not a destination, it's a journey, it's the direction in which you are traveling.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "It's what's inside you that will make you go up.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You can get what you want instead of having to want what you have.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "He was another in the endless list of thieves who steal from themselves when they try to steal from others.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "If we don't start, it's certain we can't arrive.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "When your image improves, your performance improves.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You cannot consistently perform in a manner that is inconsistent with the way you see yourself.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "The mind completes whatever picture we put in it.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "There is little you can learn from doing nothing.",
+        author: "Zig Ziglar"
+    },
+];
